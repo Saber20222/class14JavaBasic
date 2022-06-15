1d1a3b940235535e03e3dbcba222fb0b160a21b8
Saber20222 <sabersamim.ss@gmail.com>
2022-06-14 21:09:13 -0700

    class 14

--- Task4.java
+++ Task4.java
@@ -2,7 +2,7 @@ package com.syntax.class14;
 
 public class Task4 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		/*
 		 * How would you reverse a String word by word? for example
